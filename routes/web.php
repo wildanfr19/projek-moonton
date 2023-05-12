271a88ab94920442fe1a478d4476048e8b71e092
wildanfr19 <fathurrohmanwildan136@gmail.com>
2023-05-12 16:22:54 +0700

    feat: middleware check user subscription

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,9 @@ Route::redirect('/','/login');
 
 Route::middleware(['auth','role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function(){
     Route::get('/', [DashboardController::class, 'index'])->name('index');
-    Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show');
-    Route::get('subscription-plan', [SubscriptionPlanController::class, 'index'])->name('subscriptionPlan.index');
-    Route::post('subscription-plan/{subscriptionPlan}/user-subscribe', [SubscriptionPlanController::class, 'userSubscribe'])->name('subscriptionPlan.userSubscribe');
+    Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show')->middleware('CheckUserMiddlewareSubscription:true');
+    Route::get('subscription-plan', [SubscriptionPlanController::class, 'index'])->name('subscriptionPlan.index')->middleware('CheckUserMiddlewareSubscription:false');
+    Route::post('subscription-plan/{subscriptionPlan}/user-subscribe', [SubscriptionPlanController::class, 'userSubscribe'])->name('subscriptionPlan.userSubscribe')->middleware('CheckUserMiddlewareSubscription:false');
 });
 
 Route::prefix('prototype')->name('prototype.')->group(function () {
